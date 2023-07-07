8eca30d39bb348227523abcbdc386d5a86d0d2cd
DevNetoBr <138528044+DevNetoBr@users.noreply.github.com>
2023-07-07 09:48:23 -0300

    Update home.js
    Sistema de geração de senhas aleatórias.

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,13 @@
 module.exports = {
-  name: 'home', // nome do arquivo ejs referente a está página
-  run: (app, render) =>{ // renderizando a página
+  name: 'home', // Nome do arquivo .ejs referente a está página
+  run: (app, render) =>{// Renderizando a página
+
     app.get('/', (req, res) =>{
-      render(res, {nome: "José"}); // usando a função render da index.js. Essa função irá renderizar a página home .ejs
+      const crypto = require('crypto');
+      const passwordLength = 10;
+      const password = crypto.randomBytes(Math.ceil(passwordLength / 2)).toString('hex').slice(0, passwordLength);
+      render(res, {password: password}); // Usando a função render da index.js. Essa função irá renderizar a página home.ejs. Os valores dentro do objeto serão enviados para a página home.ejs
+      
     })
   }
 }
